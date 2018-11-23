77ff14970e4ec6409ae149537e29b730f5f72d4b
Franz Holzinger <holzinger@ttproducts.de>
2018-11-23 14:23:08 +0100

    add tt_content.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,19 @@
+<?php
+defined('TYPO3_MODE') or die('Access denied.');
+
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ratings' . '_pi1'] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['ratings' . '_pi1'] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ratings' . '_pi1', 'FILE:EXT:ratings/pi1/flexform_ds.xml');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    array(
+        'LLL:EXT:ratings/locallang_db.xml:tt_content.list_type_pi1',
+        'ratings',
+        'EXT:ratings/ext_icon.gif'
+    ),
+    'list_type',
+    'ratings'
+);
+
