0a578e0647448c6aa046ed0257d516b889efce4f
Franz Holzinger <holzinger@ttproducts.de>
2018-11-23 14:14:25 +0100

    * extract the file tt_news.php from ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_news.php
@@ -0,0 +1,21 @@
+<?php
+defined('TYPO3_MODE') or die('Access denied.');
+
+// New columns
+$tempColumns = array (
+    'tx_ratings_enable' => Array (
+        'exclude' => 1,
+        'label' => 'LLL:EXT:ratings/locallang_db.xml:tt_news.tx_ratings_enable',
+        'config' => array (
+            'type'     => 'check',
+            'items'    => array(
+                array('', '')
+            ),
+            'default'  => '1'
+        )
+    ),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_news', $tempColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_news', 'tx_ratings_enable;;;;1-1-1');
+
--- ext_tables.php
+++ ext_tables.php
@@ -1,30 +1,8 @@
 <?php
 if (!defined ('TYPO3_MODE')) die('Access denied.');
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
-	// New columns
-	$tempColumns = array (
-		'tx_ratings_enable' => Array (
-			'exclude' => 1,
-			'label' => 'LLL:EXT:ratings/locallang_db.xml:tt_news.tx_ratings_enable',
-			'config' => array (
-				'type'     => 'check',
-				'items'    => array(
-					array('', '')
-				),
-				'default'  => '1'
-			)
-		),
-	);
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_news', $tempColumns, 1);
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_news', 'tx_ratings_enable;;;;1-1-1');
-}
-
 $tx_ratings_sysconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ratings']);
 $tx_ratings_debug_mode_disabled = is_array($tx_ratings_sysconf) && !intval($tx_ratings_sysconf['debugMode']);
-
-
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ratings_data');
