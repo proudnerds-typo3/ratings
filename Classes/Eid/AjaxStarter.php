72202dc0c948b4ba862515e17caac82490e2fe75
Franz Holzinger <holzinger@ttproducts.de>
2018-11-23 14:09:44 +0100

    add ajax starter class file

---
+++ Classes/Eid/AjaxStarter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Netcreators\Ratings\Eid;
+
+
+class AjaxStarter {
+
+    public function run () {
+
+        \JambageCom\Div2007\Utility\FrontendUtility::init();
+
+        $eIDutility = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Frontend\Utility\EidUtility::class);
+        $eIDutility->initTCA();
+
+            // Make instance and call main():
+        /** @var Ajax $SOBE */
+        $SOBE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Netcreators\Ratings\Api\Ajax::class);
+
+        $result = $SOBE->main();
+    }
+}
