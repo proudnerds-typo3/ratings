781a73359901943119df752c120f43d6dac76969
Franz Holzinger <holzinger@ttproducts.de>
2018-11-23 14:28:52 +0100

    * move static/Ratings/constants.txt -> Configuration/TypoScript/constants.txt * move static/Ratings/setup.txt -> Configuration/TypoScript/setup.txt

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die('Access denied.');
 
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ratings_data');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ratings_iplog');
 
@@ -9,5 +8,5 @@ if (TYPO3_MODE=='BE') {
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_ratings_pi1_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ratings') . 'pi1/class.tx_ratings_pi1_wizicon.php';
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ratings', 'static/Ratings/', 'Ratings');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ratings', 'Configuration/TypoScript/', 'Ratings');
 
