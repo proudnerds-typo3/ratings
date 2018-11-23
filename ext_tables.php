66718969b2a94ca7b21dc0ca83964dc19918c38b
Franz Holzinger <holzinger@ttproducts.de>
2018-11-23 14:22:15 +0100

    * extract the file tt_content.php from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,21 +1,9 @@
 <?php
 if (!defined ('TYPO3_MODE')) die('Access denied.');
 
-$tx_ratings_sysconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ratings']);
-$tx_ratings_debug_mode_disabled = is_array($tx_ratings_sysconf) && !intval($tx_ratings_sysconf['debugMode']);
-
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ratings_data');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ratings_iplog');
-
-unset($tx_ratings_debug_mode_disabled);
-unset($tx_ratings_sysconf);
-
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['ratings' . '_pi1'] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist']['ratings' . '_pi1'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ratings' .'_pi1', 'FILE:EXT:ratings/pi1/flexform_ds.xml');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:ratings/locallang_db.xml:tt_content.list_type_pi1', 'ratings' . '_pi1'),'list_type');
 
 if (TYPO3_MODE=='BE') {
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_ratings_pi1_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ratings') . 'pi1/class.tx_ratings_pi1_wizicon.php';
