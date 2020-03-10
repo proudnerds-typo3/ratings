c14a0dcfb63171d9db4725365b1925ba4cc14a9f
Tania Morales <tania.morales@proudnerds.com>
2020-03-10 17:49:04 +0100

    [TASK] Updated constraints and version for release 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,20 +8,20 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Ratings',
 	'description' => 'Modern AJAX-based ratings system. Public free support is provided only through typo3.slack.com! Contact by e-mail for commercial support.',
 	'category' => 'plugin',
-	'version' => '3.0.0',
+	'version' => '3.0.1',
 	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearcacheonload' => 0,
-	'author' => 'Netcreators BV',
-	'author_email' => 'extensions@netcreators.com',
-	'author_company' => 'Netcreators BV',
+	'author' => 'Netcreators',
+	'author_email' => 'extensions@proudnerds.com',
+	'author_company' => 'Proud Nerds',
 	'constraints' => 
 	array (
 		'depends' => 
 		array (
-			'php' => '5.5.0-7.99.99',
-            'typo3' => '6.2.0-9.3.99'
+			'php' => '5.6.0-7.2.99',
+            'typo3' => '7.6.0-8.7.99'
 		),
 		'conflicts' => 
 		array (
