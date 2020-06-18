8efc629ba3d5178445b86f0a1402c52e7bbf97a1
Franz Holzinger <holzinger@ttproducts.de>
2020-06-18 10:46:21 +0200

    Update ext_emconf.php
    PHP 7.3 is needed

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'php' => '5.6.0-7.2.99',
+			'php' => '5.6.0-7.3.99',
             'typo3' => '7.6.0-9.5.99'
 		),
 		'conflicts' => 
