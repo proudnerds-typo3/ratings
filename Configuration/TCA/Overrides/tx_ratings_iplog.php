acf11e1697edad8d30bed25729e669941cd92791
Franz Holzinger <holzinger@ttproducts.de>
2020-06-19 09:14:17 +0200

    meet requirements for TYPO3 9.5

--- Configuration/TCA/Overrides/tx_ratings_iplog.php
+++ Configuration/TCA/Overrides/tx_ratings_iplog.php
@@ -1,11 +1,19 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-$tx_ratings_sysconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ratings']);
-$tx_ratings_debug_mode_disabled = is_array($tx_ratings_sysconf) && !intval($tx_ratings_sysconf['debugMode']);
+if (
+    defined('TYPO3_version') &&
+    version_compare(TYPO3_version, '9.0.0', '>=')
+) {
+    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    )->get('ratings');
+} else { // before TYPO3 9
+    $extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ratings']);
+}
+
+$tx_ratings_debug_mode_disabled = is_array($extensionConfiguration) && !intval($extensionConfiguration['debugMode']);
 
 $GLOBALS['TCA']['tx_ratings_iplog']['ctrl']['hideTable'] = $tx_ratings_debug_mode_disabled;
 $GLOBALS['TCA']['tx_ratings_iplog']['ctrl']['readOnly']  = $tx_ratings_debug_mode_disabled;
-
-
 
